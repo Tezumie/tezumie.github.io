2635ebb8deffd2498e8b60c1ca9759e555fcf0c5
Tezumie <102488626+Tezumie@users.noreply.github.com>
2023-03-13 10:58:10 -0600

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,117 @@
+let myImages = [
+  "imgs/asmbly.png",
+  "https://256art.s3.eu-central-1.amazonaws.com/mainnet/0xf45a8211413e78f9200958dc10e0e99786ab11d4/small/0.png",
+  "https://gateway.fxhash.xyz/ipfs/QmcYgHeuUumBZj1aW6vrDmqwp3vKh4xKgg22JvMUxMUXVZ",
+  "https://gateway.fxhash.xyz/ipfs/QmYJa2R7C5aCmSnPQarvm4kV3j8RKeDMcHRcXc8e9Y99Bc",
+  "https://gateway.fxhash.xyz/ipfs/QmPUrtigB1sLUovgYjeMrxWui7juDccsTydAHipUVb1TBq",
+  "imgs/ej.png",
+  "https://gateway.fxhash.xyz/ipfs/QmRRHo9hgSRzgN2F2ewfcrCd5bSi152ifxxhGvMzTy7XUG",
+  "imgs/trn.png",
+  "https://gateway.fxhash.xyz/ipfs/QmZBNTokbkRCdjaZqjswrCh5xiRPuMmzV8JLPVa3XxJCHQ",
+  "https://gateway.fxhash.xyz/ipfs/QmUSA8dYszGmz9QuKUwYXaMTBfmELEkbTpdCNiQrph5UAQ",
+  "https://gateway.fxhash.xyz/ipfs/QmetvQiweWBHsno6rJziweJtyP7QkTT7JPsFrnpSFuoLRV",
+  "https://gateway.fxhash.xyz/ipfs/QmShywnVKq2VT5zXnfsNEZfFAnYqVgNfvX1g8LDE9VJ1AA",
+];
+let myProjects = [
+  "",
+  "https://256art.com/project/15",
+  "https://www.fxhash.xyz/generative/12227",
+  "https://www.fxhash.xyz/generative/13189",
+  "https://www.fxhash.xyz/generative/16160",
+  "https://www.fxhash.xyz/generative/10965",
+  "https://www.fxhash.xyz/generative/14681",
+  "https://www.fxhash.xyz/generative/11594",
+  "https://www.fxhash.xyz/generative/10736",
+  "https://www.fxhash.xyz/generative/10616",
+  "https://www.fxhash.xyz/generative/17166",
+  "https://www.fxhash.xyz/generative/10287",
+];
+let imgTxt = [
+  "Assembly",
+  "Entangled",
+  "(un)existing",
+  "Riverscape",
+  "Natural order.chaos",
+  "Enlightened Journey",
+  "Lucidity",
+  "Torn",
+  "Topographic Abstraction",
+  "Hardware",
+  "Tezumie 1-1",
+  "Tezumie",
+];
+let myImagesTezumie = [
+  "tezumieimgs/ogTezumie.jpg",
+  "tezumieimgs/Sprite.png",
+  "tezumieimgs/TezumieSayan.png",
+  "tezumieimgs/POTZ.png",
+  "tezumieimgs/boredTezumie.png",
+  "tezumieimgs/cryptoTezumie.png",
+  "tezumieimgs/real.png",
+  "tezumieimgs/softTezumie.png",
+  "tezumieimgs/tezumiePOP.png",
+];
+let myProjectsTezumie = [
+  "tezumieimgs/ogTezumie.jpg",
+  "tezumieimgs/Sprite.png",
+  "tezumieimgs/TezumieSayan.png",
+  "tezumieimgs/POTZ.png",
+  "tezumieimgs/boredTezumie.png",
+  "tezumieimgs/cryptoTezumie.png",
+  "tezumieimgs/real.png",
+  "tezumieimgs/softTezumie.png",
+  "tezumieimgs/tezumiePOP.png",
+];
+let imgTxtTezumie = [
+  "Tezumie",
+  "Tezumie 1-1",
+  "Super Tezumie",
+  "POTZ Tezumie",
+  "Bored Tezumie",
+  "Tezumie Punk",
+  "Real Tezumie",
+  "Soft Tezumie",
+  "POP! Tezumie",
+];
+let myAbout = "My name is Tezumie, and I'm an artist based in the United States. I specialize in generative art, illustration, and pixel art, and I enjoy exploring different styles and techniques. My focus is on creating semi-realistic scenes and experimenting with abstract concepts, often drawing inspiration from nature.\n" +
+	"\n" +
+	"Working within a creative community is an essential part of my process, and I value the feedback and advice of my peers. For me, the artistic journey is just as meaningful as the final product, and I find motivation in collaborating with others. I'm passionate about coding with natural themes, as it presents a fun challenge and allows me to create art that resonates with others, as it is recognizable rather than abstract.\n" +
+	"\n" +
+	"Generative art has been in existence for quite some time, although it has recently gained widespread recognition. This art form employs algorithms and code to generate dynamic and distinctive pieces of art. I am captivated by its approach, which presents exciting challenges and pushes traditional art forms to their limits. The vast potential of generative art inspires me, and I eagerly anticipate witnessing its evolution over time.\n" +
+	"\n";
+let myResources = "This entire Website was made in p5js! <a href='https://editor.p5js.org'>p5js editor</a>\n" +
+	"Learn to code the same way I did, with Daniel Shiffman from the coding Train. <a href='https://www.youtube.com/thecodingtrain'>youtube series</a>\n" +
+	"Some handy functions for generative art techniques. <a href='https://github.com/Tezumie/p5js-Generative-Art-Custom-Functions'>github</a>\n" +
+	"\n";
+let assemblyTxt =
+  "<h1>Exploring the Creative Process of Generative Art<h1>\n" +
+  "My approach to creating generative artwork typically involves a well-defined idea, concept, and end goal. However, for this particular project, I decided to abandon preconceived notions and embrace an exploratory mindset. Instead of starting with a clear vision, I began by delving into the world of flow fields.\n" +
+  "\n" +
+  "<img src='z-assemblyimgs/a1.PNG'alt='example image' width='500'>\n" +
+  "\n" +
+  "<h1>Diving into Flow Fields<h1>\n" +
+  "In generative art, a flow field assigns a value or set of values to each point in a two-dimensional or three-dimensional space, representing properties like direction, velocity, density, or temperature, based on the desired visual effect.\n" +
+  "Flow fields are widely used in generative art to create dynamic and organic-looking patterns and shapes, such as swirling clouds or flowing water. To generate flow fields, artists can use various mathematical functions, including Perlin noise. Once defined, flow fields influence the movement and behavior of particles or graphic elements, creating a sense of fluidity and motion that can be captivating.\n" +
+  "Flow fields can serve as both a theme and a technique in generative art, as they are a recurring subject or motif that artists can explore and incorporate into their work, and a tool or method that artists can use to generate visual output. As a result, flow fields offer artists a versatile medium for creative expression.\n" +
+  "\n" +
+  "<img src='z-assemblyimgs/a2.PNG'alt='example image' width='500'>\n" +
+  "\n" +
+  "<h1>Technique<h1>\n" +
+  "To generate a specific vision, manipulating a flow field can pose significant challenges. For this particular project, I adopted an exploratory approach, primarily focused on achieving a smooth, flowing aesthetic commonly associated with flow fields in generative art. However, I also aimed to create a unique piece, which prompted me to develop the idea of a distance modifier.\n" +
+  "In the initial stages of the project, I experimented with distance functions, modifying the magnitude of the flow field by measuring the distance between specific points and an origin. I then applied different mathematical functions to these distances, incorporated them into the vector for the flow fields movement, and made more adjustments until I obtained the final outcome.\n" +
+  "Fine-tuning these math equations can be a tedious process. Nonetheless, experimenting with code remains one of the most enjoyable aspects of working with flow fields, as making minor modifications to math functions can result in a vast array of exciting results.\n" +
+  "<h1>Colors<h1>\n" +
+  "In selecting palettes for this project, I initially relied on my personal preference and chose colors from an RGB color selector. Subsequently, I complemented those colors with intuition until I was satisfied with the overall result. However, after receiving feedback on the colors, I realized the need for further fine-tuning.\n" +
+  "During the refinement process, I paid close attention to the cohesiveness of the palettes as a collection, which involved reducing the number of colors and precisely placing them in the artwork. This approach allowed me to achieve an aesthetic reminiscent of a canvas with color splashes, while also enhancing the sense of unity among the pieces in the collection.\n" +
+  "\n" +
+  "<img src='z-assemblyimgs/asmbly.PNG'alt='example image' width='500'>\n" +
+  "\n" +
+  "<h1>Assembly<h1>\n" +
+  "From a technical standpoint, using modified distance functions to generate the vectors of points in the flow field is a unique approach that sets it apart from traditional techniques.\n" +
+  "Conceptually, viewing flow fields as a theme rather than a technique is an intriguing departure from the norm. Starting with a theme can unlock new visual possibilities and facilitate the creation of a more cohesive and meaningful piece of art.\n" +
+  "Aesthetically, the resulting flow field produced by this approach is distinctly different from the typical flow field. The intricate patterns and illusion of shape packing create a visually striking effect that is unique to this method, further setting it apart.\n" +
+  'The interesting shapes or "entities" that comprise the artwork appear to be assembling themselves in a dynamic and cohesive manner. This interpretation inspired the selection of the title, which aptly captures the essence of the work and invites viewers to interpret it in their own unique way.\n' +
+  "\n" +
+  "<img src='z-assemblyimgs/a3.PNG'alt='example image' width='500'>\n" +
+  "\n" +
+  "\n";
---
+++ sketch.js
@@ -0,0 +1,535 @@
+let garbageCan = [];
+let headerHeight,
+  borderWidth,
+  footerHeight,
+  bodyWidth,
+  bodyHeight,
+  bodyContentWidth,
+  bodyContentHeight;
+let p, palette;
+let theme = 0;
+let PageTitles = ["Home", "Series", "About", "Resources"];
+let currentPage = 0;
+let currentSection = 0;
+let pageBodyDiv;
+let allItemsDiv;
+function setup() {
+  Palette();
+  createCanvas(window.innerWidth, window.innerHeight * 1.1);
+  document.body.style.overflowX = "hidden";
+  headerHeight = 60;
+  footerHeight = window.innerHeight * 0.1;
+  borderWidth = width / 78;
+  bodyWidth = width - borderWidth * 2;
+  bodyContentWidth = width - borderWidth * 2 - p.bodypadding * 2;
+  bodyHeight = height - headerHeight - footerHeight;
+  bodyContentHeight = height - headerHeight - footerHeight - p.bodypadding * 2;
+  drawElements();
+  windowResized();
+}
+function drawElements() {
+  background(p.backGround);
+  if (currentPage == 0) {
+    AllItemsContainer();
+    allItemsDiv.child(header());
+    addBannerImage(allItemsDiv, 500);
+    PageBodyContainer();
+
+    pageBodyDiv.child(
+      createTextBox(
+        assemblyTxt,
+        0,
+        0,
+        bodyContentWidth,
+        bodyContentHeight * 1.5,
+        "center"
+      )
+    );
+    allItemsDiv.child(pageBodyDiv);
+    allItemsDiv.child(footer());
+    allItemsDiv.style("height", "auto");
+    let pageBodyHeight = pageBodyDiv.elt.offsetHeight;
+  }
+  if (currentPage == 1 && currentSection != 1) {
+    AllItemsContainer();
+    allItemsDiv.child(header());
+    PageBodyContainer();
+    allItemsDiv.child(pageBodyDiv);
+
+    let buttonDiv = createDiv("");
+    buttonDiv.id("AllItems");
+    buttonDiv.id("buttonContainer");
+    pageBodyDiv.child(buttonDiv);
+    gridImages(buttonDiv, myImages, myProjects, imgTxt, 2);
+
+    allItemsDiv.child(footer());
+    pageBodyDiv.style("height", "auto");
+    allItemsDiv.style("height", "auto");
+    let pageBodyHeight = pageBodyDiv.elt.offsetHeight;
+  }
+  if (currentPage == 1 && currentSection == 1) {
+    AllItemsContainer();
+    allItemsDiv.child(header());
+    addBannerImage(allItemsDiv, 500);
+    PageBodyContainer();
+    pageBodyDiv.child(
+      createTextBox(
+        assemblyTxt,
+        0,
+        0,
+        bodyContentWidth,
+        bodyContentHeight * 1.5,
+        "center"
+      )
+    );
+    allItemsDiv.child(pageBodyDiv);
+    allItemsDiv.child(footer());
+    allItemsDiv.style("height", "auto");
+    let pageBodyHeight = pageBodyDiv.elt.offsetHeight;
+  }
+  if (currentPage == 2) {
+    AllItemsContainer();
+    allItemsDiv.child(header());
+    PageBodyContainer();
+    allItemsDiv.child(pageBodyDiv);
+    pageBodyDiv.child(
+      createTextBox(
+        myAbout,
+        0,
+        0,
+        bodyContentWidth,
+        bodyContentHeight * 1.5,
+        "left"
+      )
+    );
+    let buttonDiv = createDiv("");
+    buttonDiv.id("AllItems");
+    buttonDiv.id("buttonContainer");
+    pageBodyDiv.child(buttonDiv);
+    gridImages(buttonDiv, myImagesTezumie, myProjectsTezumie, imgTxtTezumie, 3);
+
+    allItemsDiv.child(footer());
+    pageBodyDiv.style("height", "auto");
+    allItemsDiv.style("height", "auto");
+    let pageBodyHeight = pageBodyDiv.elt.offsetHeight;
+  }
+  if (currentPage == 3) {
+    AllItemsContainer();
+    allItemsDiv.child(header());
+    PageBodyContainer();
+    allItemsDiv.child(pageBodyDiv);
+    pageBodyDiv.child(
+      createTextBox(
+        myResources,
+        0,
+        0,
+        bodyContentWidth,
+        bodyContentHeight * 1.5,
+        "left"
+      )
+    );
+    allItemsDiv.child(footer());
+    pageBodyDiv.style("height", "auto");
+    allItemsDiv.style("height", "auto");
+    let pageBodyHeight = pageBodyDiv.elt.offsetHeight;
+  }
+}
+function addBannerImage(allItemsDiv, bannerHeight) {
+  let bannerDiv = createDiv();
+  bannerDiv.id("AllItems");
+  bannerDiv.style("position", "relative");
+  bannerDiv.style("width", "100%");
+  bannerDiv.style("height", `${bannerHeight}px`);
+  let bannerImg = createImg("imgs/homepg.png", "");
+  bannerImg.id("AllItems");
+  bannerImg.style("position", "absolute");
+  bannerImg.style("width", "100%");
+  bannerImg.style("height", `${bannerHeight}px`);
+  bannerImg.style("object-fit", "cover");
+  let logoImg = createImg("imgs/assemblylogo.png", "");
+  logoImg.id("AllItems");
+  logoImg.style("position", "absolute");
+  logoImg.style("left", "50%");
+  logoImg.style("transform", "translateX(-50%)");
+  logoImg.style("top", `${(bannerHeight - 150) / 2}px`);
+  logoImg.style("height", "160px");
+  bannerDiv.child(bannerImg);
+  bannerDiv.child(logoImg);
+  allItemsDiv.child(bannerDiv);
+}
+
+function gridImages(buttonDiv, imgs, mylinks, mytext, cols) {
+  buttonDiv.style("display", "grid");
+  buttonDiv.style("grid-template-columns", "repeat(" + cols + ", 1fr)");
+  buttonDiv.style("grid-gap", "20px");
+  buttonDiv.style("position", "relative");
+  buttonDiv.style("margin-top", "20px");
+  buttonDiv.style("padding", "20px 20px");
+  buttonDiv.style("aspect-ratio", "1/1");
+
+  for (let i = 0; i < 12; i++) {
+    let buttons = createButton("");
+    buttons.id("imgButtons");
+    buttons.style("height", "auto");
+    buttons.style("border", "none");
+    buttons.style("border-radius", "5px");
+    buttons.style("padding", "15px");
+    buttons.style("text-align", "center");
+    buttons.style("aspect-ratio", "1/1");
+    buttons.style("background-image", "url(" + imgs[i] + ")");
+    buttons.style("background-size", "contain");
+    buttons.style("background-position", "center");
+    buttons.style("background-repeat", "no-repeat");
+    buttonDiv.child(buttons);
+    let textDiv = createDiv(mytext[i]);
+    textDiv.id("imgText");
+    textDiv.style("font-family", p.fontFamily);
+    textDiv.style("text-align", "center");
+    textDiv.style("max-width", buttons.elt.clientWidth - 30 + "px");
+    textDiv.style("padding", "4px");
+    textDiv.style("margin-right", "-50px");
+    textDiv.style("white-space", "nowrap");
+    textDiv.style("text-overflow", "ellipsis");
+    textDiv.style("overflow", "hidden");
+    textDiv.style("position", "relative");
+    buttons.child(textDiv);
+    styleElement(buttons, textDiv);
+    buttons.mouseClicked(() => {
+      if (i + 1 == 1) {
+        currentSection = i + 1;
+        windowResized();
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      } else {
+        window.open(mylinks[i], "_blank");
+      }
+    });
+  }
+}
+
+function AllItemsContainer() {
+  allItemsDiv = createDiv("");
+  allItemsDiv.id("AllItems");
+  allItemsDiv.style("position", "absolute");
+  allItemsDiv.style("top", headerHeight - 1 + "px");
+  allItemsDiv.style("height", "auto");
+  allItemsDiv.style("width", "100%");
+  allItemsDiv.style("display", "flex");
+  allItemsDiv.style("flex-direction", "column");
+  allItemsDiv.style("align-items", "center");
+  allItemsDiv.style("background-color", p.backGround);
+}
+function PageBodyContainer() {
+  pageBodyDiv = createDiv("");
+  pageBodyDiv.id("pageBody");
+  pageBodyDiv.style("box-sizing", "border-box");
+  pageBodyDiv.style("position", "relative");
+  pageBodyDiv.style("top", 0 + "px");
+  pageBodyDiv.style("height", "auto");
+  pageBodyDiv.style("width", bodyWidth + "px");
+  pageBodyDiv.style("line-height", "1.5");
+  pageBodyDiv.style("padding", p.bodypadding + "px");
+  pageBodyDiv.style("overflow-x", "auto");
+  styleElement(pageBodyDiv);
+}
+
+function createTextBox(text, x, y, w, h, allignment) {
+  if (x + w > bodyContentWidth) {
+    w = bodyContentWidth - x;
+  }
+  if (x < 0) {
+    x = 0;
+  }
+  let textDiv = createDiv("");
+  textDiv.id("text");
+  textDiv.style("box-sizing", "border-box");
+  textDiv.style("position", "relative");
+  textDiv.style("top", y + "px");
+  textDiv.style("height", "auto");
+  textDiv.style("width", w + "px");
+  textDiv.style("left", x + "px");
+  textDiv.style("padding", "30px");
+  textDiv.style("line-height", "1.5");
+  styleElement(textDiv);
+  let lines = text.split("\n");
+  for (let i = 0; i < lines.length; i++) {
+    let line = lines[i];
+    let lineDiv = createDiv("");
+    lineDiv.id("text");
+    lineDiv.parent(textDiv);
+    lineDiv.style("display", "flex");
+    lineDiv.style("flex-direction", "row");
+    lineDiv.style("align-items", "center");
+    lineDiv.style("justify-content", allignment);
+    let match = /<img.*?>/g.exec(line);
+    if (match) {
+      let beforeText = line.slice(0, match.index);
+      if (beforeText) {
+        let beforeDiv = createDiv(beforeText);
+        beforeDiv.id("text");
+        beforeDiv.parent(lineDiv);
+      }
+      let imageDiv = createDiv(match[0]);
+      imageDiv.id("imgText");
+      imageDiv.style("margin-top", "30px");
+      imageDiv.style("margin-bottom", "30px");
+      imageDiv.parent(lineDiv);
+      let afterText = line.slice(match.index + match[0].length);
+      if (afterText) {
+        let afterDiv = createDiv(afterText);
+        afterDiv.id("text");
+        afterDiv.parent(lineDiv);
+      }
+    } else {
+      let titleMatch = /<h(\d)>(.*?)<\/h\1>/g.exec(line);
+      if (titleMatch) {
+        let titleDiv = createDiv(titleMatch[2]);
+        titleDiv.id("text");
+        titleDiv.parent(textDiv);
+        styleElement(titleDiv);
+      } else {
+        let textDiv = createDiv(line);
+        textDiv.id("text");
+        textDiv.style("margin-bottom", "20px");
+        textDiv.parent(lineDiv);
+        styleElement(textDiv);
+      }
+      let h1Tags = document.getElementsByTagName("h1");
+      for (let i = 0; i < h1Tags.length; i++) {
+        h1Tags[i].style.fontFamily = p.fontFamily;
+        h1Tags[i].style.fontSize = "18px";
+      }
+    }
+  }
+
+  let links = textDiv.elt.querySelectorAll("a");
+  for (let i = 0; i < links.length; i++) {
+    links[i].style.color = p.linkColor;
+    links[i].style.textDecoration = "underline";
+  }
+
+  let images = textDiv.elt.querySelectorAll("img");
+  for (let i = 0; i < images.length; i++) {
+    images[i].style.maxWidth = "100%";
+  }
+  return textDiv;
+}
+function header() {
+  let headerDiv = createDiv("");
+  headerDiv.id("header");
+  headerDiv.style("height", headerHeight + "px");
+  headerDiv.style("box-sizing", "border-box");
+  headerDiv.style("position", "fixed");
+  headerDiv.style("top", "0");
+  headerDiv.style("width", "100%");
+  headerDiv.style("z-index", "100");
+  styleElement(headerDiv);
+  let buttonDiv = createDiv("");
+  buttonDiv.id("buttonContainer");
+  headerDiv.child(buttonDiv);
+  drawPageButtons(buttonDiv);
+}
+function footer() {
+  let footerDiv = createDiv("");
+  footerDiv.id("footer");
+  footerDiv.style("position", "relative");
+  footerDiv.style("bottom", 0);
+  footerDiv.style("height", "100px");
+  footerDiv.style("width", "100%");
+  styleElement(footerDiv);
+  return footerDiv;
+}
+function drawPageButtons(buttonDiv) {
+  buttonDiv.style("display", "flex");
+  buttonDiv.style("justify-content", "flex-start");
+  buttonDiv.style("left", borderWidth + "px");
+  buttonDiv.style("position", "absolute");
+  buttonDiv.style("margin-top", "18px");
+  for (let i = 0; i < PageTitles.length; i++) {
+    let buttons = createButton(PageTitles[i]);
+    buttons.id("pageButtons");
+    buttons.style("height", "25px");
+    buttons.style("margin", "0 5px");
+    buttons.style("border", "none");
+    buttons.style("border-radius", "5px");
+    buttons.style("padding", "0px 8px");
+    buttons.style("padding-bottom", "2px");
+    buttons.style("text-align", "center");
+    styleElement(buttons, i);
+    buttons.mouseClicked(() => {
+      currentPage = i;
+      currentSection = 0;
+      windowResized();
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    });
+    buttonDiv.child(buttons);
+  }
+}
+function styleElement(myElement, page) {
+  if (myElement.elt.id === "text") {
+    myElement.style("background-color", p.textBox);
+    myElement.style("font-family", p.fontFamily);
+    myElement.style("color", p.textColor);
+    myElement.style("font-size", p.textSize + "px");
+  }
+  if (myElement.elt.id === "pageBody") {
+    myElement.style("background-color", p.pageBody);
+  }
+  if (myElement.elt.id === "header") {
+    myElement.style("background-color", p.headerColor);
+  }
+  if (myElement.elt.id === "footer") {
+    myElement.style("background-color", p.footerColor);
+  }
+  if (myElement.elt.id === "pageButtons") {
+    myElement.style("font-family", p.fontFamily);
+    myElement.style("font-size", p.buttonTextSize + "px");
+    if (page == currentPage) {
+      myElement.elt.style.backgroundColor = p.pageButtonHover;
+      myElement.elt.style.color = p.pageButtonHoverTxt;
+    } else {
+      myElement.elt.style.backgroundColor = p.pageButtons;
+      myElement.elt.style.color = p.pageButtonTxt;
+    }
+    myElement.elt.addEventListener("mouseover", () => {
+      myElement.style("cursor", "pointer");
+      myElement.elt.style.backgroundColor = p.pageButtonHover;
+      myElement.elt.style.color = p.pageButtonHoverTxt;
+    });
+    myElement.elt.addEventListener("mouseout", () => {
+      myElement.style("cursor", "default");
+      if (page == currentPage) {
+        myElement.elt.style.backgroundColor = p.pageButtonHover;
+        myElement.elt.style.color = p.pageButtonHoverTxt;
+      } else {
+        myElement.elt.style.backgroundColor = p.pageButtons;
+        myElement.elt.style.color = p.pageButtonTxt;
+      }
+    });
+  }
+  if (myElement.elt.id === "imgButtons") {
+    page.style("font-family", p.fontFamily);
+    page.style("letter-spacing", "1px");
+    page.style("font-size", "0vw");
+    page.style("color", "white");
+    page.style(
+      "text-shadow",
+      "1px 1px 0px #000, -1px -1px 0px #000, 1px -1px 0px #000, -1px 1px 0px #000"
+    );
+    myElement.elt.style.backgroundColor = p.pageButtons;
+    myElement.elt.style.color = p.pageButtonTxt;
+    myElement.elt.addEventListener("mouseover", () => {
+      page.style("font-size", "2.0vw");
+      // myElement.elt.style.backgroundColor = "rgb(0,0,0,0.25)";
+      myElement.elt.style.color = p.pageButtonHoverTxt;
+      myElement.style("cursor", "pointer");
+      myElement.style("transform", "scale(1.01)");
+      myElement.style("filter", "brightness(92%)");
+    });
+    myElement.elt.addEventListener("mouseout", () => {
+      page.style("font-size", "0vw");
+      myElement.elt.style.backgroundColor = p.pageButtons;
+      myElement.elt.style.color = p.pageButtonTxt;
+      myElement.style("cursor", "default");
+      myElement.style("transform", "scale(1.0)");
+      myElement.style("filter", "brightness(100%)");
+    });
+  }
+}
+function windowResized() {
+  let currentScroll = window.pageYOffset;
+  let element;
+
+  element = select("#AllItems");
+  if (element) {
+    garbageCan.push(element);
+  }
+  element = select("#header");
+  if (element) {
+    garbageCan.push(element);
+  }
+  element = select("#footer");
+  if (element) {
+    garbageCan.push(element);
+  }
+  element = select("#text");
+  if (element) {
+    garbageCan.push(element);
+  }
+  element = select("#buttonContainer");
+  if (element) {
+    garbageCan.push(element);
+  }
+  element = select("#imgText");
+  if (element) {
+    garbageCan.push(element);
+  }
+  element = select("#pageBody");
+  if (element) {
+    garbageCan.push(element);
+  }
+  element = select("#pageButtons");
+  if (element) {
+    garbageCan.push(element);
+  }
+  for (let i = 0; i < garbageCan.length; i++) {
+    garbageCan[i].remove();
+  }
+  let buttons = selectAll("button");
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].remove();
+  }
+  garbageCan = [];
+  resizeCanvas(window.innerWidth, window.innerHeight * 1.1);
+  headerHeight = 60;
+  footerHeight = window.innerHeight * 0.1;
+  borderWidth = width / 7;
+  bodyWidth = width - borderWidth * 2;
+  bodyContentWidth = width - borderWidth * 2 - p.bodypadding * 2;
+  bodyHeight = height - headerHeight - footerHeight;
+  bodyContentHeight = height - headerHeight - footerHeight - p.bodypadding * 2;
+  drawElements();
+  window.scrollTo(0, currentScroll);
+}
+function Palette() {
+  palette = [
+    {
+      Theme: "Dark",
+      fontFamily: "Tahoma, Arial, sans-serif",
+      backGround: color(15),
+      bodypadding: 1,
+      pageBody: color(16),
+      textBox: color(13),
+      textColor: color(225),
+      textSize: 16,
+      linkColor: color(0, 150, 255),
+      headerColor: color(18),
+      footerColor: color(18),
+      pageButtons: color(0, 0),
+      pageButtonTxt: color(225),
+      pageButtonHover: color(40, 180),
+      pageButtonHoverTxt: color(225),
+      buttonTextSize: 16,
+    },
+    {
+      Theme: "Light",
+      fontFamily: "Tahoma, Arial, sans-serif",
+      backGround: color(255),
+      bodypadding: 5,
+      pageBody: color(200),
+      textBox: color(255, 0, 0),
+      headerColor: color(225),
+      footerColor: color(225),
+      pageButtons: color(0),
+      pageButtonTxt: color(255),
+      pageButtonHover: color(255),
+      pageButtonHoverTxt: color(0),
+    },
+  ];
+  p = palette[theme];
+}
